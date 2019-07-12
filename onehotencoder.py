21aeac3ea51a234986502deb57b3aae037504bde
kat-drz <44135985+kat-drz@users.noreply.github.com>
2019-07-12 09:32:31 +0100

    Add files via upload

---
+++ onehotencoder.py
@@ -0,0 +1,26 @@
+#possible input values
+bases = "ATCG"
+
+#mapping of bases to integers as a dictionary
+base_to_integer = dict((i, c) for c, i in enumerate(bases))
+print(base_to_integer)
+
+#input sequence to be one hot encoded
+sequence = "AGATATAC"
+
+#encoding input sequence as integers
+integer_encoded = [base_to_integer[y] for y in sequence]
+print(integer_encoded)
+
+#one hot encoding
+onehot_encoded = list()
+for value in integer_encoded:
+    base = [0 for x in range(len(bases))]
+    base[value] = 1
+    onehot_encoded.append(base)
+    
+print(onehot_encoded)
+
+
+
+      
