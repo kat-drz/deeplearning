572326b8bff222e8bfb28090fd26459726cacab7
kat-drz <44135985+kat-drz@users.noreply.github.com>
2019-07-15 11:50:24 +0100

    Update onehotencoder.py

--- onehotencoder.py
+++ onehotencoder.py
@@ -1,26 +1,36 @@
-#possible input values
+#list of all possible bases
 bases = "ATCG"
 
-#mapping of bases to integers as a dictionary
-base_to_integer = dict((i, c) for c, i in enumerate(bases))
-print(base_to_integer)
-
-#example input sequence to be one hot encoded
+#input sequence for one hot encoding
 sequence = "AGATATAC"
 
-#encoding input sequence as integers
-integer_encoded = [base_to_integer[y] for y in sequence]
-print(integer_encoded)
+#list of undesirable letters in the input sequence
+bad_list = ["N","a","t", "g", "c"]
 
-#one hot encoding
-onehot_encoded = list()
-for value in integer_encoded:
-    base = [0 for x in range(len(bases))]
-    base[value] = 1
-    onehot_encoded.extend(base)
-    
-print(onehot_encoded)
+#checking for presence of any of the components in bad_list in sequence
+result = [component for component in bad_list if(component in sequence)]
 
+#if undesirable letters are present in sequence, an error message is displayed
+if (bool(result)) == True:
+    print("The input sequence is invalid.")
+    print("Please check for lowercase letters and/or 'N'.")
+  
+#if sequence is in the correct format, proceed with one hot encoding
+else:
+     #mapping of bases to integers as a dictionary
+    base_to_integer = dict((i, c) for c, i in enumerate(bases))
+
+    #encoding input sequence as integers
+    integer_encoded = [base_to_integer[y] for y in sequence]
+
+    #one hot encoding
+    onehot_encoded = list()
+    for value in integer_encoded:
+        base = [0 for x in range(len(bases))]
+        base[value] = 1
+        onehot_encoded.extend(base)
+    print(onehot_encoded)
+   
 
 
       
