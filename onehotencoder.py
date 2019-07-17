428d46bd0e48aee2a727b1d2b69224f936926aec
kat-drz <44135985+kat-drz@users.noreply.github.com>
2019-07-17 15:01:54 +0100

    Update onehotencoder.py
    One-hot-encoder with sequence validity check, which can take multi-line .txt or fasta files as input.

--- onehotencoder.py
+++ onehotencoder.py
@@ -1,32 +1,44 @@
-#list of all possible bases
-bases = "ATCG"
+path = 'dnasequence.txt'
+file = open(path, 'r')
 
-#input sequence for one hot encoding
-sequence = "AGATATAC"
+holder = file.read()
+print(holder)
+
+#remove all sequence numbers from all lines
+import re
+test = re.sub('1|2|3|4|5|6|7|8|9|0|>|s|e|q|:', "", holder)
+newone = test.split("\n")
+print(newone)
 
 #list of undesirable letters in the input sequence
-bad_list = ["N","a","t", "g", "c"]
+bad_list = ["N", "a", "t", "g", "c"]
 
 #checking for presence of any of the components in bad_list in sequence
-result = [component for component in bad_list if(component in sequence)]
-
+result = [component for component in bad_list if(component in newone)]
+    
 #if undesirable letters are present in sequence, an error message is displayed
-if (bool(result)) == True:
-    print("The input sequence is invalid.")
-    print("Please check for lowercase letters and/or 'N'.")
+
+for line in newone:
+
+    if (bool(result)) == True:
+        print("The input sequence is invalid.")
+        print("Please check for lowercase letters and/or 'N'.")
   
 #if sequence is in the correct format, proceed with one hot encoding
-else:
+
+    else:   
      #mapping of bases to integers as a dictionary
-    base_to_integer = dict((i, c) for c, i in enumerate(bases))
+        bases = "ATCG"
+        base_to_integer = dict((i, c) for c, i in enumerate(bases))
 
     #encoding input sequence as integers
-    integer_encoded = [base_to_integer[y] for y in sequence]
+    
+        integer_encoded = [base_to_integer[y] for y in line]
 
     #one hot encoding
-    onehot_encoded = list()
-    for value in integer_encoded:
-        base = [0 for x in range(len(bases))]
-        base[value] = 1
-        onehot_encoded.extend(base)
-    print(onehot_encoded)
+        onehot_encoded = list()
+        for value in integer_encoded:
+            base = [0 for x in range(len(bases))]
+            base[value] = 1
+            onehot_encoded.extend(base)
+        print(onehot_encoded)
